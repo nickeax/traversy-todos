b9cc3a32f7b69325e85599f6c57c1c2f385021a3
Nick Fletcher <nickeax@gmail.com>
2021-05-01 14:00:00 +1000

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TodosComponent } from "./components/todos/todos.component";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  name: string = "Nick Fletcher";
   title = 'angular-crash';
+
+  constructor () {
+    this.changeName("Hello Angular");
+  }
+
+  changeName(text: string): void {
+    var tmp = this.name;
+    
+    this.name = `The old name was ${tmp}, it's now ${text}`;
+  }
 }
